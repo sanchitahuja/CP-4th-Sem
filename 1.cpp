d84611f157c3d6024c197cf71f58f8526c770107
MohMaya <shivanshuchaudhary97@gmail.com>
2017-03-07 20:37:11 +0530

    1 modified

--- 1.cpp
+++ 1.cpp
@@ -1,2 +1,32 @@
 //programmes to calculate largest of 3 numbers
 #include <stdio.h>
+int main() {
+        float a,b,c;
+        printf("Enter the three Numbers\n");
+        scanf("%d %d %d",&a,&b,&c);
+        printf("\n");
+        if (a>b)
+        {
+                if (a>c)
+                {
+                        printf("%lf is the largest\n",a);
+                }
+                else
+                {
+                        printf("%lf is the largest\n",c);
+                }
+        }
+        else if(b>a)
+        {
+                if (b>c)
+                {
+                        printf("%lf is the largest\n",b);
+                }
+                else
+                {
+                        printf("%fl is the largest\n",c);
+                }
+        }
+
+        return 0;
+}
